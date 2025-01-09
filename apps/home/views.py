7e9a8c8bf7704722121bc827b1a55db3f60fb26c
Кирилл Грибанов <gribanov.k@mamod.ai>
2025-01-09 19:45:11 +0300

    merge fix

--- apps/home/views.py
+++ apps/home/views.py
@@ -1012,7 +1012,7 @@ def chat(request):
 
 def chat_messages(request):
     chat_id = request.GET.get('chat_id', None)
-    user_id = request.GET.get('user_id', None)
+    client_id = request.GET.get('user_id', None)
     if chat_id is not None:
         current_chat = Chat.objects.filter(id=chat_id).first()
     else:
@@ -1060,7 +1060,7 @@ def chat_messages(request):
         'chats': chats,
         'messages': messages,
         'chat': current_chat,
-        'projects': Project.objects.filter(client_id=user_id)
+        'projects': Project.objects.filter(client_id=client_id)
     }
 
     return render(request, 'apps/chat.html', context)
