37a3dd3c426f438e5c8fc21325bf24fd1f8e8b55
Кирилл Грибанов <gribanov.k@mamod.ai>
2025-01-11 11:43:01 +0300

    fix get status

--- apps/telegram/prsr.py
+++ apps/telegram/prsr.py
@@ -134,7 +134,7 @@ def save_messages(user_id, messages, project, channel, user_view_name):
             try:
                 text_status = assistant.ask_chat_status()
                 print(f"Chat status is  {text_status}")
-                statuses = dict(Chat.CHAT_STATUS);
+                statuses = dict(Chat.CHAT_STATUS)
                 if statuses[text_status] is not None:
                     chat.status = text_status
                     chat.save()
