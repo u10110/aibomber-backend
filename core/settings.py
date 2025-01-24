ea8815644bff0177d664e0dd06efd67a110d4c12
Кирилл Грибанов <gribanov.k@mamod.ai>
2025-01-24 16:12:42 +0300

    add csrf setting for dev

--- core/settings.py
+++ core/settings.py
@@ -297,6 +297,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'https://eliment.ai',
     'https://dev.eliment.ai',
-    'https://neurounit.ai'
+    'https://neurounit.ai',
+    'https://dev.neurounit.ai'
 ]
 
