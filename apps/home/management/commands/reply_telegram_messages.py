ccae9106eb123d39456a10180a3ca24c6deb93d0
Грибанов Кирилл <gribanov_ks@crc.ru>
2025-01-29 21:08:48 +0300

    fix  reply

--- apps/home/management/commands/reply_telegram_messages.py
+++ apps/home/management/commands/reply_telegram_messages.py
@@ -66,7 +66,7 @@ class Command(BaseCommand):
 
                         time.sleep(10)
                         message_processor = MessageProcessor()
-                        if Project.per_conversation_limit < message_processor.chat_messages_count(chat):
+                        if int(Project.per_conversation_limit) < int(message_processor.chat_messages_count(chat)):
                             ProjectProcessor.process_chat(chat, message_processor)
                         else:
                             logger.info(f"Достигнут лимит сообщений по чату {chat.user_id}")
