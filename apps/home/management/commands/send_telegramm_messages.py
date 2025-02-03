1c1200c58856e4e32196aa47f498bc3ce368adfa
Грибанов Кирилл <gribanov_ks@crc.ru>
2025-02-03 19:21:43 +0300

    asdasd

--- apps/home/management/commands/send_telegramm_messages.py
+++ apps/home/management/commands/send_telegramm_messages.py
@@ -14,7 +14,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         while True:
             try:
-                new_chat_messages(124)
+                new_chat_messages()
                 time.sleep(10)
             except Exception as e:
                 logger.error(e)
