6fec130626955cf00ebfe2bf119a67a22d0a385c
Грибанов Кирилл <gribanov_ks@crc.ru>
2025-02-06 13:25:05 +0300

    env

--- apps/home/management/commands/reply_telegram_messages.py
+++ apps/home/management/commands/reply_telegram_messages.py
@@ -77,6 +77,7 @@ class Command(BaseCommand):
                         logger.debug(project.per_conversation_limit)
                         logger.debug(message_processor.chat_messages_count(chat))
                         if project.per_conversation_limit > message_processor.chat_messages_count(chat):
+                            logger.info(f"Отправка ответа {chat.user_id}")
                             ProjectProcessor.process_chat(chat, message_processor)
                         else:
                             logger.info(f"Достигнут лимит сообщений по чату {chat.user_id}")
